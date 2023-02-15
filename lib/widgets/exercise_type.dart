db674741120e59d64e6123dcafa0fea401998a10
Alex Lomia <alex.lomia@gmail.com>
2023-02-16 01:47:07 +0400

    Add exercise_type.dart

---
+++ lib/widgets/exercise_type.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class ExerciseType extends StatelessWidget {
+  static const width = 144.0;
+  static const height = 80.0;
+
+  final String name;
+
+  const ExerciseType({
+    Key? key,
+    required this.name,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: const BoxDecoration(
+        color: Color.fromRGBO(231, 224, 236, 1),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 16,
+            offset: Offset(0, 4),
+          ),
+        ],
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(8),
+          bottomRight: Radius.circular(8),
+        ),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(right: 8),
+                  child: const Icon(
+                    Icons.drag_indicator,
+                    color: Color.fromRGBO(103, 80, 164, 1),
+                  ),
+                ),
+                Text(
+                  name,
+                  style: Theme.of(context).textTheme.labelSmall,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

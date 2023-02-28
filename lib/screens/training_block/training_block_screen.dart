aa893008a5c224b775fb14d37d5893c0e5aaaa02
Alex Lomia <alex.lomia@gmail.com>
2023-02-28 14:16:39 -0500

    Add logic to training_block_screen.dart

--- lib/screens/training_block/training_block_screen.dart
+++ lib/screens/training_block/training_block_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:journal_flutter/models_client/exercise_day_client.dart';
 import 'package:journal_flutter/services/normalize_data_service.dart';
+import 'package:journal_flutter/widgets/exercise_day_widget.dart';
 import 'package:journal_flutter/widgets/layout.dart';
 
 import '../../utils/print_json.dart';
@@ -18,41 +20,63 @@ class TrainingBlockScreen extends StatefulWidget {
 }
 
 class _TrainingBlockScreenState extends State<TrainingBlockScreen> {
+  late final Future<List<ExerciseDayClient>> normalizedData;
+
   @override
   void initState() {
     super.initState();
 
-    NormalizeDataService()
+    normalizedData = NormalizeDataService()
         .getNormalizedData(trainingBlockId: widget.trainingBlockId)
         .then((value) {
       for (final exerciseDay in value) {
         printJson(exerciseDay.toJson());
       }
-    });
-  }
 
-  @override
-  void dispose() {
-    // trainingBlockState.dispose();
-    // normalizeDataState.dispose();
-    super.dispose();
+      return value;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Layout(
-      child: Center(
-        child: Column(
-          children: [
-            Text('Training Block: ${widget.trainingBlockId}'),
-            ElevatedButton(
-              onPressed: () => GoRouter.of(context).go('/'),
-              child: const Text('Go back'),
-            )
-          ],
-        ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Training Block: ${widget.trainingBlockId}'),
+          ElevatedButton(
+            onPressed: () => GoRouter.of(context).go('/'),
+            child: const Text('Go back'),
+          ),
+          FutureBuilder(
+            future: normalizedData,
+            builder: (context, snapshot) {
+              final exerciseDays = snapshot.data;
+
+              if (exerciseDays == null) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  for (final exerciseDay in exerciseDays)
+                    Container(
+                      key: Key(exerciseDay.id),
+                      margin: const EdgeInsets.only(bottom: 24.0),
+                      child: ExerciseDayWidget(
+                        name: exerciseDay.name,
+                        exerciseTypes: exerciseDay.exerciseTypes,
+                      ),
+                    ),
+                ],
+              );
+            },
+          ),
+        ],
       ),
-      // child: ExerciseDayWidget(name: 'Push 1'),
     );
   }
 }

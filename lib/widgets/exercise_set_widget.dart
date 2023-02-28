1e116798a237b5c9c39bc47408718f0bfd4eb618
Alex Lomia <alex.lomia@gmail.com>
2023-02-28 14:13:55 -0500

    Add exercise_set_widget.dart and exercise_widget.dart

---
+++ lib/widgets/exercise_set_widget.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:journal_flutter/models_client/exercise_set_client.dart';
+
+import 'exercise_type_widget.dart';
+
+class ExerciseSetWidget extends StatelessWidget {
+  static const width = 75.0;
+  static const borderRadius = Radius.circular(8);
+  static const borderSide = BorderSide(color: Colors.black12, width: 1);
+
+  final bool isSingle;
+  final bool isLeftmost;
+  final bool isRightmost;
+  final ExerciseSetClient exerciseSet;
+
+  get _borderTopCell {
+    return const Border(right: borderSide, bottom: borderSide);
+  }
+
+  get _borderBottomCell {
+    final rightSide = isSingle || isRightmost ? BorderSide.none : borderSide;
+
+    return Border(right: rightSide);
+  }
+
+  const ExerciseSetWidget({
+    Key? key,
+    this.isSingle = false,
+    this.isLeftmost = false,
+    this.isRightmost = false,
+    required this.exerciseSet,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const backgroundColor = Color.fromRGBO(231, 224, 236, 1);
+
+    return SizedBox(
+      width: width,
+      height: ExerciseTypeWidget.height,
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: backgroundColor,
+              border: _borderTopCell,
+            ),
+            height: ExerciseTypeWidget.height / 2,
+            child: Center(
+              child: Text(
+                exerciseSet.reps,
+                style: Theme.of(context).textTheme.labelLarge,
+              ),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: backgroundColor,
+              border: _borderBottomCell,
+            ),
+            height: ExerciseTypeWidget.height / 2,
+            child: Center(
+              child: Text(
+                exerciseSet.load,
+                style: Theme.of(context).textTheme.labelLarge,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/exercise_widget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:journal_flutter/models_client/exercise_client.dart';
+import 'package:journal_flutter/widgets/exercise_set_widget.dart';
+
+class ExerciseWidget extends StatelessWidget {
+  static const borderRadius = Radius.circular(8);
+
+  final ExerciseClient exercise;
+
+  const ExerciseWidget({
+    Key? key,
+    required this.exercise,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(borderRadius),
+      ),
+      clipBehavior: Clip.hardEdge,
+      child: Row(children: [
+        for (final entry in exercise.exerciseSets.asMap().entries)
+          ExerciseSetWidget(
+            key: UniqueKey(),
+            exerciseSet: entry.value,
+            isSingle: exercise.exerciseSets.length == 1,
+            isLeftmost: entry.key == 0,
+            isRightmost: entry.key == exercise.exerciseSets.length - 1,
+          ),
+      ]),
+    );
+  }
+}

40e11026d62e2a13ea95c509a125abaec6ac4140
Alex Lomia <alex.lomia@gmail.com>
2023-03-01 15:19:59 -0500

    Add proper themes

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,19 +1,112 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+final colorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromRGBO(103, 80, 164, 1),
+);
+
+final textTheme = TextTheme(
+  displayLarge: GoogleFonts.robotoTextTheme().displayLarge!.copyWith(
+        fontWeight: FontWeight.w400,
+        fontSize: 57,
+        height: 1.12,
+        color: Colors.black,
+      ),
+  displayMedium: GoogleFonts.robotoTextTheme().displayMedium!.copyWith(
+        fontWeight: FontWeight.w400,
+        fontSize: 45,
+        height: 1.16,
+        color: Colors.black,
+      ),
+  displaySmall: GoogleFonts.robotoTextTheme().displaySmall!.copyWith(
+        fontWeight: FontWeight.w400,
+        fontSize: 36,
+        height: 1.22,
+        color: Colors.black,
+      ),
+  headlineLarge: GoogleFonts.robotoTextTheme().displaySmall!.copyWith(
+        fontWeight: FontWeight.w400,
+        fontSize: 32,
+        height: 1.25,
+        color: Colors.black,
+      ),
+  headlineMedium: GoogleFonts.robotoTextTheme().headlineMedium!.copyWith(
+        fontWeight: FontWeight.w400,
+        fontSize: 28,
+        height: 1.29,
+        color: Colors.black,
+      ),
+  headlineSmall: GoogleFonts.robotoTextTheme().headlineSmall!.copyWith(
+        fontWeight: FontWeight.w400,
+        fontSize: 24,
+        height: 1.33,
+        color: Colors.black,
+      ),
+  titleLarge: GoogleFonts.robotoTextTheme().bodyLarge!.copyWith(
+        fontWeight: FontWeight.w400,
+        fontSize: 22,
+        height: 1.27,
+        color: Colors.black,
+      ),
+  titleMedium: GoogleFonts.robotoTextTheme().bodyMedium!.copyWith(
+        fontWeight: FontWeight.w500,
+        fontSize: 16,
+        height: 1.5,
+        color: Colors.black,
+        letterSpacing: 0.15,
+      ),
+  titleSmall: GoogleFonts.robotoTextTheme().bodySmall!.copyWith(
+        fontWeight: FontWeight.w500,
+        fontSize: 14,
+        height: 1.43,
+        color: Colors.black,
+        letterSpacing: 0.1,
+      ),
+  labelLarge: GoogleFonts.robotoTextTheme().bodyLarge!.copyWith(
+        fontWeight: FontWeight.w500,
+        fontSize: 14,
+        height: 1.43,
+        color: Colors.black,
+        letterSpacing: 0.1,
+      ),
+  labelMedium: GoogleFonts.robotoTextTheme().bodyMedium!.copyWith(
+        fontWeight: FontWeight.w500,
+        fontSize: 12,
+        height: 1.33,
+        color: Colors.black,
+        letterSpacing: 0.5,
+      ),
+  labelSmall: GoogleFonts.robotoTextTheme().labelSmall!.copyWith(
+        fontWeight: FontWeight.w500,
+        fontSize: 11,
+        height: 1.45,
+        color: Colors.black,
+        letterSpacing: 0.5,
+      ),
+  bodyLarge: GoogleFonts.robotoTextTheme().bodyLarge!.copyWith(
+        fontWeight: FontWeight.w400,
+        fontSize: 16,
+        height: 1.5,
+        color: Colors.black,
+        letterSpacing: 0.5,
+      ),
+  bodyMedium: GoogleFonts.robotoTextTheme().bodyMedium!.copyWith(
+        fontWeight: FontWeight.w400,
+        fontSize: 14,
+        height: 1.43,
+        color: Colors.black,
+        letterSpacing: 0.25,
+      ),
+  bodySmall: GoogleFonts.robotoTextTheme().bodySmall!.copyWith(
+        fontWeight: FontWeight.w400,
+        fontSize: 12,
+        height: 1.33,
+        color: Colors.black,
+        letterSpacing: 0.4,
+      ),
+);
+
 final theme = ThemeData(
-  colorSchemeSeed: const Color.fromRGBO(103, 80, 164, 1),
-  textTheme: TextTheme(
-    titleLarge: GoogleFonts.robotoTextTheme().bodyLarge!.copyWith(
-          fontSize: 22,
-          height: 1.27,
-          color: const Color.fromRGBO(28, 27, 31, 1),
-        ),
-    labelSmall: GoogleFonts.robotoTextTheme().labelSmall!.copyWith(
-          fontWeight: FontWeight.w500,
-          fontSize: 11,
-          height: 1.45,
-          color: const Color.fromRGBO(73, 69, 79, 1),
-        ),
-  ),
+  colorScheme: colorScheme,
+  textTheme: textTheme,
 );

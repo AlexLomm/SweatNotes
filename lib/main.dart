9049c770b21ef776b02cc92ec6b88266c6f3ee02
Alex Lomia <alex.lomia@gmail.com>
2023-03-01 20:25:58 -0500

    Fix _registerErrorHandlers();

--- lib/main.dart
+++ lib/main.dart
@@ -43,12 +43,14 @@ void _registerErrorHandlers() {
 
   // Show some error UI when any widget in the app fails to build
   ErrorWidget.builder = (FlutterErrorDetails details) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: const Text('An error occurred'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: const Text('An error occurred'),
+        ),
+        body: Center(child: Text(details.toString())),
       ),
-      body: Center(child: Text(details.toString())),
     );
   };
 }

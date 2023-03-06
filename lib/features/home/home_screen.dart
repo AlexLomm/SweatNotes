dca87e92b966b00e0aee6e95386f33b31b3edfa4
Alex Lomia <alex.lomia@gmail.com>
2023-03-06 15:09:50 -0500

    Add rive animation for empty state on home page

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:rive/rive.dart';
 
 import '../../widgets/button.dart';
 import '../../widgets/layout.dart';
@@ -23,6 +24,12 @@ class HomeScreen extends ConsumerWidget {
         // TODO: dispose stream?
         stream: trainingBlocksService.trainingBlocks,
         builder: (context, snapshot) {
+          final appBarHeight = Scaffold.of(context).appBarMaxHeight ?? 0;
+          final safeAreaHeight = mq.size.height -
+              mq.padding.top -
+              mq.padding.bottom -
+              appBarHeight;
+
           final data = snapshot.data;
 
           if (data == null) {
@@ -30,55 +37,58 @@ class HomeScreen extends ConsumerWidget {
           }
 
           return SizedBox(
-            height: mq.size.height,
+            height: safeAreaHeight,
             width: mq.size.width,
-            child: ListView(
-              children: [
-                for (final trainingBlock
-                    in snapshot.data as List<TrainingBlock>)
-                  Container(
-                    key: Key(trainingBlock.id),
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    margin: const EdgeInsets.only(bottom: 8.0),
-                    child: Button(
-                      backgroundColor:
-                          Theme.of(context).colorScheme.primaryContainer,
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 16.0,
-                        horizontal: 24.0,
-                      ),
-                      borderRadius: 12,
-                      label: trainingBlock.name,
-                      onPressed: () => context.go('/${trainingBlock.id}'),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            trainingBlock.name,
-                            style: Theme.of(context)
-                                .textTheme
-                                .titleLarge
-                                ?.copyWith(
+            child: data.isEmpty
+                // TODO: add dark mode
+                ? const RiveAnimation.asset(
+                    alignment: Alignment.center,
+                    'empty-state-home-light.riv',
+                  )
+                : ListView(children: [
+                    for (final trainingBlock in data)
+                      Container(
+                        key: Key(trainingBlock.id),
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        margin: const EdgeInsets.only(bottom: 8.0),
+                        child: Button(
+                          backgroundColor:
+                              Theme.of(context).colorScheme.primaryContainer,
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 16.0,
+                            horizontal: 24.0,
+                          ),
+                          borderRadius: 12,
+                          label: trainingBlock.name,
+                          onPressed: () => context.go('/${trainingBlock.id}'),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                trainingBlock.name,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleLarge
+                                    ?.copyWith(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onPrimaryContainer,
+                                    ),
+                              ),
+                              Transform.rotate(
+                                angle: pi,
+                                child: Icon(
+                                  Icons.arrow_back,
                                   color: Theme.of(context)
                                       .colorScheme
                                       .onPrimaryContainer,
                                 ),
+                              )
+                            ],
                           ),
-                          Transform.rotate(
-                            angle: pi,
-                            child: Icon(
-                              Icons.arrow_back,
-                              color: Theme.of(context)
-                                  .colorScheme
-                                  .onPrimaryContainer,
-                            ),
-                          )
-                        ],
+                        ),
                       ),
-                    ),
-                  ),
-              ],
-            ),
+                  ]),
           );
         },
       ),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import cloud_firestore
 import firebase_auth
 import firebase_core
 import path_provider_foundation
+import rive_common
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -16,5 +17,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }

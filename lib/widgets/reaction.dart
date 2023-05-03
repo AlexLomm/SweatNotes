dd2fb050efc340eea79321e12aa1438f38ae55be
Alex Lomia <alex.lomia@gmail.com>
2023-05-02 21:22:50 -0400

    Add reaction.dart and reaction_menu.dart widgets

---
+++ lib/widgets/reaction.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../features/training_block/widget_params.dart';
+
+class Reaction extends ConsumerWidget {
+  final bool isSelected;
+  final bool isCancelState;
+  final int? value;
+
+  Reaction({
+    Key? key,
+    this.isSelected = false,
+    this.isCancelState = false,
+    required this.value,
+  })  : assert(value == null || [-5, 0, 5].contains(value)),
+        super(key: key);
+
+  // account for the issues regarding centering of emojis on iOS
+  // see: https://github.com/flutter/flutter/issues/119623
+  get _emojiByValue {
+    if (isCancelState) return const EmojiCancel();
+
+    switch (value) {
+      case -5:
+        return const EmojiBad();
+      case 0:
+        return const EmojiNeutral();
+      case 5:
+        return const EmojiGood();
+      default:
+        return const EmojiNone();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final widgetParams = ref.watch(widgetParamsProvider);
+
+    return Container(
+      width: widgetParams.reactionCircleSize,
+      height: widgetParams.reactionCircleSize,
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surface,
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: isSelected
+              ? Theme.of(context).colorScheme.primary.withOpacity(0.67)
+              : Theme.of(context).colorScheme.onSurface.withOpacity(0.32),
+          width: 1.0,
+        ),
+      ),
+      child: Center(
+        child: _emojiByValue,
+      ),
+    );
+  }
+}
+
+class EmojiGood extends ConsumerWidget {
+  const EmojiGood({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final widgetParams = ref.watch(widgetParamsProvider);
+
+    return Transform.translate(
+      offset: widgetParams.isCompactMode ? const Offset(1.35, 0.0) : const Offset(0.5, -0.5),
+      child: Text(
+        '🤩',
+        textAlign: TextAlign.center,
+        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+              fontSize: widgetParams.isCompactMode ? 18.0 : 22.0,
+            ),
+      ),
+    );
+  }
+}
+
+class EmojiNeutral extends ConsumerWidget {
+  const EmojiNeutral({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final widgetParams = ref.watch(widgetParamsProvider);
+
+    return Transform.translate(
+      offset: widgetParams.isCompactMode ? const Offset(1.4, -0.75) : const Offset(0.5, -1.0),
+      child: Text(
+        '😐',
+        textAlign: TextAlign.center,
+        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+              fontSize: widgetParams.isCompactMode ? 18.0 : 22.0,
+            ),
+      ),
+    );
+  }
+}
+
+class EmojiBad extends ConsumerWidget {
+  const EmojiBad({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final widgetParams = ref.watch(widgetParamsProvider);
+
+    return Transform.translate(
+      offset: widgetParams.isCompactMode ? const Offset(1.0, -0.5) : const Offset(0.0, -0.5),
+      child: Text(
+        '🤮',
+        textAlign: TextAlign.center,
+        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+              fontSize: widgetParams.isCompactMode ? 18.0 : 22.0,
+            ),
+      ),
+    );
+  }
+}
+
+class EmojiNone extends ConsumerWidget {
+  const EmojiNone({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final widgetParams = ref.watch(widgetParamsProvider);
+
+    return Transform.translate(
+      offset: widgetParams.isCompactMode ? const Offset(2.0, -1.0) : const Offset(0.4, -1.3),
+      child: Text(
+        '🫥',
+        textAlign: TextAlign.center,
+        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+              fontSize: widgetParams.isCompactMode ? 16.0 : 22.0,
+            ),
+      ),
+    );
+  }
+}
+
+class EmojiCancel extends ConsumerWidget {
+  const EmojiCancel({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final widgetParams = ref.watch(widgetParamsProvider);
+
+    return Transform.translate(
+      offset: widgetParams.isCompactMode ? const Offset(0.0, 0.0) : const Offset(0.0, 0.0),
+      child: Text(
+        '✕',
+        textAlign: TextAlign.center,
+        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+              fontSize: widgetParams.isCompactMode ? 16.0 : 22.0,
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/reaction_menu.dart
@@ -0,0 +1,293 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../features/training_block/widget_params.dart';
+import 'reaction.dart';
+
+class ReactionMenu extends ConsumerStatefulWidget {
+  final int? selectedReaction;
+  final Duration animationDuration;
+  final Curve animationCurve;
+  final Function(int? value) onSelect;
+
+  const ReactionMenu({
+    Key? key,
+    this.selectedReaction,
+    required this.animationDuration,
+    required this.animationCurve,
+    required this.onSelect,
+  }) : super(key: key);
+
+  @override
+  ConsumerState createState() => _ReactionMenuState();
+}
+
+class _ReactionMenuState extends ConsumerState<ReactionMenu> with SingleTickerProviderStateMixin {
+  int? selectedReaction;
+
+  late GlobalKey _key;
+  late Offset _buttonPosition;
+  late Size _buttonSize;
+  OverlayEntry? _menuOverlayEntry;
+  OverlayEntry? _backdropOverlayEntry;
+  late AnimationController _animationController;
+  late Animation<double> _opacityAnimation;
+  late Animation<double> _expandAnimation;
+
+  get isCompletedOrAnimatingForward =>
+      _animationController.status == AnimationStatus.completed ||
+      _animationController.status == AnimationStatus.forward;
+
+  get isDismissedOrAnimatingReverse =>
+      _animationController.status == AnimationStatus.dismissed ||
+      _animationController.status == AnimationStatus.reverse;
+
+  get isDismissed => _animationController.status == AnimationStatus.dismissed;
+
+  @override
+  void didUpdateWidget(ReactionMenu oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (widget.selectedReaction != selectedReaction) {
+      setState(() => selectedReaction = widget.selectedReaction);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    setState(() => selectedReaction = widget.selectedReaction);
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: widget.animationDuration,
+    )..addStatusListener((status) {
+        if (status == AnimationStatus.dismissed) _removeOverlays();
+      });
+
+    _opacityAnimation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: widget.animationCurve,
+      ),
+    );
+
+    _expandAnimation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: widget.animationCurve,
+      ),
+    );
+
+    _key = GlobalKey();
+  }
+
+  void _removeOverlays() {
+    _menuOverlayEntry?.remove();
+    _menuOverlayEntry = null;
+
+    _backdropOverlayEntry?.remove();
+    _backdropOverlayEntry = null;
+  }
+
+  @override
+  void dispose() {
+    _removeOverlays();
+    _animationController.dispose();
+
+    super.dispose();
+  }
+
+  void _closeMenu({bool giveHapticFeedback = false}) {
+    if (isDismissedOrAnimatingReverse) return;
+
+    if (giveHapticFeedback) {
+      HapticFeedback.mediumImpact();
+    }
+
+    _animationController.reverse();
+  }
+
+  void _selectReaction(int? value) {
+    final updatedValue = value == selectedReaction ? null : value;
+
+    _closeMenu(giveHapticFeedback: true);
+
+    setState(() => selectedReaction = updatedValue);
+
+    widget.onSelect(updatedValue);
+  }
+
+  void _openMenu() {
+    if (isCompletedOrAnimatingForward) return;
+
+    final renderBox = _key.currentContext?.findRenderObject() as RenderBox;
+
+    _buttonSize = renderBox.size;
+    _buttonPosition = renderBox.localToGlobal(Offset.zero);
+
+    if (_menuOverlayEntry == null && _backdropOverlayEntry == null) {
+      final backdropOverlayEntry = _backdropOverlayEntryBuilder();
+      final menuOverlayEntry = _menuOverlayEntryBuilder();
+
+      _backdropOverlayEntry = backdropOverlayEntry;
+      _menuOverlayEntry = menuOverlayEntry;
+
+      Overlay.of(context).insert(backdropOverlayEntry);
+      Overlay.of(context).insert(menuOverlayEntry);
+    }
+
+    HapticFeedback.mediumImpact();
+
+    _animationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (context, child) => Container(
+        key: _key,
+        child: GestureDetector(
+          onTap: () {
+            // only handle opening the menu, closing is handled separately.
+            // Also, `isDismissed` check is needed to cover an edge case when
+            // the button is clicked through the backdrop
+            if (isDismissed) _openMenu();
+          },
+          child: Transform.scale(
+            scale: 1 + 0.25 * _expandAnimation.value,
+            child: Opacity(
+              opacity: 1.0 - _expandAnimation.value.clamp(0.0, 1.0),
+              child: Reaction(value: widget.selectedReaction),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  OverlayEntry _backdropOverlayEntryBuilder() {
+    return OverlayEntry(
+      builder: (context) {
+        return Positioned.fill(
+          child: Listener(
+            behavior: HitTestBehavior.translucent,
+            onPointerDown: (event) => _closeMenu(),
+          ),
+        );
+      },
+    );
+  }
+
+  OverlayEntry _menuOverlayEntryBuilder() {
+    return OverlayEntry(
+      builder: (context) {
+        final widgetParams = ref.watch(widgetParamsProvider);
+
+        const containerHeightBase = 110.0;
+        const containerWidthBase = 75.0;
+
+        final containerHeight = widgetParams.isCompactMode ? containerHeightBase : 1.25 * containerHeightBase;
+        final containerWidth = widgetParams.isCompactMode ? containerWidthBase : 1.25 * containerWidthBase;
+
+        return Positioned(
+          top: _buttonPosition.dy + _buttonSize.height / 2 - containerHeight / 2,
+          left: _buttonPosition.dx + _buttonSize.width - containerWidth,
+          child: AnimatedBuilder(
+            animation: _animationController,
+            builder: (context, child) {
+              return Opacity(
+                opacity: _opacityAnimation.value.clamp(0, 1.0),
+                child: SizedBox(
+                  height: containerHeight,
+                  width: containerWidth,
+                  child: Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Transform(
+                          origin: Offset(
+                            widgetParams.reactionCircleSize / 2,
+                            widgetParams.reactionCircleSize / 2,
+                          ),
+                          transform: Matrix4.identity()..scale(1.0 * _expandAnimation.value),
+                          child: GestureDetector(
+                            onTap: () => _closeMenu(giveHapticFeedback: true),
+                            child: Opacity(
+                              opacity: _expandAnimation.value.clamp(0, 1.0),
+                              child: Reaction(isCancelState: true, value: null),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Transform(
+                          origin: Offset(
+                            widgetParams.reactionCircleSize / 2,
+                            widgetParams.reactionCircleSize / 2,
+                          ),
+                          transform: Matrix4.identity()
+                            ..scale(1.25 * _expandAnimation.value)
+                            ..translate(-0.16 * containerWidth * _expandAnimation.value, -0.25 * containerHeight, 0),
+                          child: GestureDetector(
+                            onTap: () => _selectReaction(5),
+                            child: Reaction(
+                              value: 5,
+                              isSelected: selectedReaction == 5,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Transform(
+                          origin: Offset(
+                            widgetParams.reactionCircleSize / 2,
+                            widgetParams.reactionCircleSize / 2,
+                          ),
+                          transform: Matrix4.identity()
+                            ..scale(1.25 * _expandAnimation.value)
+                            ..translate(-0.4 * containerWidth * _expandAnimation.value, 0, 0),
+                          child: GestureDetector(
+                            onTap: () => _selectReaction(0),
+                            child: Reaction(
+                              value: 0,
+                              isSelected: selectedReaction == 0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Transform(
+                          origin: Offset(
+                            widgetParams.reactionCircleSize / 2,
+                            widgetParams.reactionCircleSize / 2,
+                          ),
+                          transform: Matrix4.identity()
+                            ..scale(1.25 * _expandAnimation.value)
+                            ..translate(-0.16 * containerWidth * _expandAnimation.value, 0.25 * containerHeight, 0),
+                          child: GestureDetector(
+                            onTap: () => _selectReaction(-5),
+                            child: Reaction(
+                              value: -5,
+                              isSelected: selectedReaction == -5,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+}
